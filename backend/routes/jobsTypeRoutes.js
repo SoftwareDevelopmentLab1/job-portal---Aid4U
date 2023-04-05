5e68dcf671eaaf29d8798a78047e7496069b3cb5
pmittal26 <pmittal2626@gmail.com>
2023-04-05 11:27:13 -0400

    corretction

--- backend/routes/jobsTypeRoutes.js
+++ backend/routes/jobsTypeRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createJobType, allJobType, updateJobType, deleteJobType } = require("../controllers/jobsTypeController");
+const { createJobType, allJobsType, updateJobType, deleteJobType } = require("../controllers/jobsTypeController");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 const router= express.Router();
 
@@ -7,7 +7,7 @@ const router= express.Router();
 router.post('/type/create', isAuthenticated, createJobType)
 
 //for all jobs
-router.get('/type/jobs', allJobType)
+router.get('/type/jobs', allJobsType)
 // upadte
 router.put('/type/update/:type_id',isAuthenticated, isAdmin, updateJobType)
 
